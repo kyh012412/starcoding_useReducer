dc552db8d9acfe4a6627a27310c8445f31712a9a
kyh012412 <kyh0124.12@gmial.com>
2024-03-06 20:30:41 +0900

    ex1end

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # 별코딩 useReducer
+컴포넌트에 스테이트 생성관리 useState를 썻었음
+useReducer도 일종의 관리
+```json
+{
+  "teacher":"james",
+  "students":["Kim","Ann","John"],
+  "count":3,
+  "location": [
+    {"country":"Korea","name":"A"},
+    {"country":"Australia","name":"B"},
+  ]
+}
+```
+같은 여러개의 하위값을 포함하는 복잡한 state를 다뤄야할때 유용
+useState대신 useReducer를 사용하면 코드를 훨씬 깔끔하게 쓸수있으며 유지보수도 쉬워짐
+
+### 그전에 3가지 개념을 알아야함
+* Reducer
+* Dispatch
+* Action
+![useReducer설명이미지](./public/img/useReducer설명이미지2.png)
+
+예금,출금,송금 등의 Action을 첨부 할 수 있음
+
+![useReducer설명이미지](./public/img/useReducer설명이미지3.png)
--- src/App.js
+++ src/App.js
@@ -1,8 +1,55 @@
+import { useReducer, useState } from 'react';
 import './App.css';
 
+//reducer - state를 업데이트 하는 역할 (은행)
+//dispatch - state 업데이트를 위한 요구
+//action - 요구의 내용
+
+const ACTION_TYPES = {
+  deposit:'deposit',
+  withdraw:'withdraw'
+}
+
+//state는 reducer가 불리는 시점에 money의 값을 가지게됨
+const reducer = (state,action) => {
+  //..
+  // console.log('reducer가 일을 합니다!',state,action);
+  switch(action.type){
+    case ACTION_TYPES.deposit:
+      return state + action.payload;
+    case ACTION_TYPES.withdraw:
+      return state - action.payload;
+    default :
+      return state;
+  }
+}
+
 function App() {
+  const [number,setNumber] = useState(0);
+  //useReducer 2개의 매개변수를 받는데 reducer와 money의 초기값이다.
+  const [money,dispatch] = useReducer(reducer,0);
+
   return (
-    <div></div>
+    <div>
+      <h2>useReducer 은행에 오신것을 환영합니다.</h2>
+      <p> 잔고: {money}원</p>
+      <input
+        type='number'
+        value={number}
+        onChange={e=>setNumber(parseInt(e.target.value))}
+        step="1000"
+      />
+      <button onClick={()=>{
+        //action은 보통 object 형태로 보냄
+        //그안에 type과 payload를 같이 보냄
+        dispatch({type:ACTION_TYPES.deposit,payload: number})
+      }}>예금</button>
+      <button onClick={()=>{
+        //action은 보통 object 형태로 보냄
+        //그안에 type과 payload를 같이 보냄
+        dispatch({type:ACTION_TYPES.withdraw,payload: number})
+      }}>출금</button>
+    </div>
   );
 }
 
