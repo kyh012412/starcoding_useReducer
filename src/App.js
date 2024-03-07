30fd62c3e23de47765089a3bfcf85665ac8c7397
kyh012412 <kyh0124.12@gmial.com>
2024-03-07 11:44:25 +0900

    커스텀코드 과제실패1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useReducer, useState } from 'react';
+import { useEffect, useReducer, useState } from 'react';
 import './App.css';
 
 //reducer - state를 업데이트 하는 역할 (은행)
@@ -6,8 +6,8 @@ import './App.css';
 //action - 요구의 내용
 
 const ACTION_TYPES = {
-  deposit:'deposit',
-  withdraw:'withdraw'
+  add:'add',
+  delete:'delete'
 }
 
 //state는 reducer가 불리는 시점에 money의 값을 가지게됨
@@ -15,40 +15,43 @@ const reducer = (state,action) => {
   //..
   // console.log('reducer가 일을 합니다!',state,action);
   switch(action.type){
-    case ACTION_TYPES.deposit:
-      return state + action.payload;
-    case ACTION_TYPES.withdraw:
-      return state - action.payload;
+    case ACTION_TYPES.add:
+      return [...state,action.payload];
+    case ACTION_TYPES.delete:
+      return state;
     default :
       return state;
   }
 }
 
 function App() {
-  const [number,setNumber] = useState(0);
-  //useReducer 2개의 매개변수를 받는데 reducer와 money의 초기값이다.
-  const [money,dispatch] = useReducer(reducer,0);
+  const [students,dispatch] = useReducer(reducer,[]);
+  const [input,setInput] = useState('');
+
+  useEffect(()=>{
+    console.log(input);
+  })
 
   return (
     <div>
-      <h2>useReducer 은행에 오신것을 환영합니다.</h2>
-      <p> 잔고: {money}원</p>
+      <h2>출석부</h2>
+      <h3> 총 학생 수: {students.length}</h3>      
       <input
-        type='number'
-        value={number}
-        onChange={e=>setNumber(parseInt(e.target.value))}
-        step="1000"
+        type='text'
+        value={input}
+        onChange={e=>setInput(e.target.value)}
       />
       <button onClick={()=>{
         //action은 보통 object 형태로 보냄
         //그안에 type과 payload를 같이 보냄
-        dispatch({type:ACTION_TYPES.deposit,payload: number})
-      }}>예금</button>
-      <button onClick={()=>{
-        //action은 보통 object 형태로 보냄
-        //그안에 type과 payload를 같이 보냄
-        dispatch({type:ACTION_TYPES.withdraw,payload: number})
-      }}>출금</button>
+        dispatch({type:ACTION_TYPES.add,payload:input})
+      }}>추가</button>
+      <div>
+        {students.map((student,idx)=>{
+          return (<h3>{student}</h3>
+          <button onClick={()=>{dispatch({type:'delete',payload:student})}}>삭제</button>)
+        })}
+      </div>
     </div>
   );
 }
